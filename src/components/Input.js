bd1e20fd75f0b40a047c428df2f14c1fc4b75688
Petyo Petrov <Petyo.Petrov@tick42.com>
2019-07-23 19:49:17 +0300

    refactor(task:8): change readme

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React from 'react'
 
-
-export  const Input = ({onChange, ...rest})=>{
+export const Input = ({ onChange, ...rest }) => {
     return <input onChange={onChange} {...rest}/>
 }
