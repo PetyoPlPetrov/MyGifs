6834ca9f5ed338b51d92275721d21224dcaf364b
Petyo Petrov <Petyo.Petrov@tick42.com>
2019-07-24 10:05:24 +0300

    refactor(task:10): refactor row cols

--- src/components/Headbar.js
+++ src/components/Headbar.js
@@ -1,26 +1,25 @@
 import React from 'react'
-import {Button, Col, Container, Row} from 'reactstrap'
-import {Input} from "./Input";
-import {isEmpty} from "ramda";
+import {
+    Button,
+    Container,
+} from 'reactstrap'
+import { Input } from './Input'
+import { isEmpty } from 'ramda'
 
-export const HeadBar = ({urls, searchedGif, onInputChange, onStartSearchClick, onToggleColumns}) => {
+export const HeadBar = ({ urls, searchedGif, onInputChange, onStartSearchClick, onToggleColumns }) => {
     return <Container>
-        <Row>
-            <Col><h1>My Giff</h1>
-            </Col>
-        </Row>
-        <Row>
-            <Col> <Input className='search' placeholder='Search for giffs...'
-                         onChange={onInputChange}
-                         value={searchedGif}/></Col>
-            <Col>
-
+        <h1>My Giff</h1>
+        <div className='header'>
+            <Input className='search' placeholder='Search for giffs...'
+                   onChange={onInputChange}
+                   value={searchedGif}/>
+            <div className='header-buttons'>
                 <Button color='primary' onClick={onStartSearchClick}
                         disabled={isEmpty(searchedGif)}>Search!!</Button>
                 <Button color='secondary' className='toggle-test' onClick={onToggleColumns}
                         disabled={isEmpty(urls)}>Toggle columns
                 </Button>
-            </Col>
-        </Row>
+            </div>
+        </div>
     </Container>
 }
