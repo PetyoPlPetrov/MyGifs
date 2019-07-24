690b225f5ec1e34e60b64e6585ddaeca0329d324
Petyo Petrov <Petyo.Petrov@tick42.com>
2019-07-24 10:47:06 +0300

    refactor(task:11): format code

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -5,13 +5,16 @@ import {
 } from 'ramda'
 
 
-const Gif = memoizeWith((urls) => toString(urls), ({ initialPageState, urls, ...rest }) => {
+const Gif = memoizeWith(toString, ({ initialPageState, urls, ...rest }) => {
     if (!initialPageState && urls.length === 0) {
         return <h1>Empty Results</h1>
     }
     return <>
-        {urls.map(e => <div className={'gif'} key={e.url}><img alt={e.title} src={e.url} title={e.title} {...rest}/>
-        </div>)}
+        {urls.map(e =>
+            <div className={'gif'} key={e.url}>
+                <img alt={e.title} src={e.url} title={e.title} {...rest}/>
+            </div>)
+        }
     </>
 })
 
